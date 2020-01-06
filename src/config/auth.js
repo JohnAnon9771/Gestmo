39078458a6eb9d2bac7f2117ad8e5a5b4209354b
JohnAnon9771 <njoao97710@gmail.com>
2020-01-06 13:27:45 -0300

    added authenticate jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '8aa713055ddd9cfcf543f9d95385a1db',
+  experesIn: '7d'
+};
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,23 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import uploadConfig from './config/upload';
-
 import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
+import SessionController from './app/controllers/SessionController';
+
+import uploadConfig from './config/upload';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = Router();
 const upload = multer(uploadConfig);
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
+
 routes.get('/users', UserController.index);
+
+routes.use(authMiddleware);
 
 routes.post('/files', upload.single('avatar'), FileController.store);
 
